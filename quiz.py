80b09c1a631718effe89ed4544aec74aed98f1b6
24halls-ops <24halls@mountainview.school.nz>
2026-04-23 13:19:39 +1200

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,8 @@
+import time
+user_name=input("What is your name ")
+time.sleep(1)
+print("Hello "+user_name+"")
+time.sleep(1)
+age = int(input("What is your age"))
+if age >= 14:
+ print("sorry you arent in the right age group for this quiz")
