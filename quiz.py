8966e8bf73d316224f438ee36cfa25768809f0f7
24halls-ops <24halls@mountainview.school.nz>
2026-04-30 13:13:12 +1200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -9,7 +9,7 @@ if age >= 14:
 if age <= 7:
   print("Sorry you aren't in the right age group for this quiz")
 
-#all instructions
+#all instructions on how this quiz works
 else:
  time.sleep(2)
  print("Welcome to this quiz you are going to be ask 3 qustions")
@@ -31,7 +31,10 @@ print()
 question = ["Q1 Someone sends you a text that is hurtful and makes you feel bad about yourself."]
 answer = []
 for q in question :
- answer = input (q)
+ answer = input ("they")
+
+
+
 
 
 
