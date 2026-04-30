786f61029c50ed42f8706d19a74dd114c31d41c2
24halls-ops <24halls@mountainview.school.nz>
2026-04-30 12:32:37 +1200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -3,4 +3,32 @@ user_name=input("What is your name ")
 time.sleep(1)
 print("Hello "+user_name+"")
 time.sleep(1)
+age = int(input("What is your age "))
 if age >= 14:
+  print("Sorry you aren't in the right age group for this quiz")
+if age <= 7:
+  print("Sorry you aren't in the right age group for this quiz")
+
+else:
+ time.sleep(2)
+ print("Welcome to this quiz you are going to be ask 3 qustions")
+ time.sleep(2)
+ print ()
+ print("Ever time you get a qustion right on your frist try you will get a point")
+ time.sleep(3)
+ print ()
+ print("you will have 3 chances to get the question right if you dont get it right you will be told the answer.")
+ time.sleep(2)
+ print ()
+ print ("Your score is currently 0")
+ time.sleep(2)
+print()
+sumbit = input ("write anything to say you understand ")
+print()
+question = ["Q1 Someone sends you a text that is hurtful and makes you feel bad about yourself."]
+answer = []
+for q in question :
+ answer = input (q)
+
+
+
